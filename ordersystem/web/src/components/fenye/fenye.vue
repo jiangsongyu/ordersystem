1586f7432cc158bdda56d6e0e7df88d12f0fd658
jiangsongyu <245968148@qq.com>
2017-10-30 22:16:45 +0800

    jsy6

--- ordersystem/web/src/components/fenye/fenye.vue
+++ ordersystem/web/src/components/fenye/fenye.vue
@@ -22,29 +22,69 @@
                    var nav = $('<nav/>').attr({ariaLabel: 'Page navigation'});
                    var ul = $('<ul/>').addClass('pagination');
                    
-                   var html = `<li>
-                     <a aria-label="Previous">
-                       <span aria-hidden="true">&laquo;</span>
-                     </a>
-                   </li>`;
-                   ul.append(html);
+                   // var html = `<li>
+                   //   <a class="previous" aria-label="Previous">
+                   //     <span aria-hidden="true">&laquo;</span>
+                   //   </a>
+                   // </li>`;
+                   // ul.append(html);
                    for(var i=0; i < 10; i++){
                        var li = $('<li/>');
                        var a = $('<a/>').text(i+1);
                        a.appendTo(li);
                        li.appendTo(ul);
                    }
-                   var html1 = `<li>
-                         <a aria-label="Next">
-                           <span aria-hidden="true">&raquo;</span>
-                         </a>
-                       </li>`;
+                   // var html1 = `<li>
+                   //       <a class="next" aria-label="Next">
+                   //         <span aria-hidden="true">&raquo;</span>
+                   //       </a>
+                   //     </li>`;
 
-                   ul.append(html1);
+                   // ul.append(html1);
                    ul.appendTo(nav);
 
                    nav.appendTo($('.lyy-content'));
-                   console.log(nav)
+                   ul.children('li').eq(1).addClass('active');
+                   ul.on('click','li',function(){
+                        $(this).addClass('active').siblings('li').removeClass('active');
+                        var num = ($(this).text())*1;
+                        if($(this).children('a').hasClass('previous')){
+                            // num++;
+                            // $.ajax({
+                            //     url: 'http://localhost:88/fenye',
+                            //     type: 'GET',
+                            //     data: {'pageNo': num, 'qty':10},
+                            //     success:function(res){
+                            //         console.log(res)
+                            //     }
+                            // })
+                            
+                        } else if($(this).children('a').hasClass('next')){
+                            // num--;
+                            // $.ajax({
+                            //     url: 'http://localhost:88/fenye',
+                            //     type: 'GET',
+                            //     data: {'pageNo': num, 'qty':10},
+                            //     success:function(res){
+                            //         console.log(res)
+                            //     }
+                            // })
+                            
+                        } else{
+                            $.ajax({
+                                url: 'http://localhost:88/fenye',
+                                type: 'GET',
+                                data: {'pageNo': num, 'qty':10},
+                                success:function(res){
+                                    console.log(res)
+                                }
+                            })
+                        }
+                       
+                        
+                        
+                   })
+
                }
            }) 
         }
